3e958dfb44d0fa80fa9055b099bf40b8897ccb0c
Flavio-Converso <fla.converso@gmail.com>
2024-08-01 04:31:18 +0200

    Add password hashing to AuthService
    - Added `System.Security.Cryptography` and `System.Text` namespaces to `AuthService.cs`.
    - Implemented `HashPassword` method in `AuthService` for SHA-256 hashing.
    - Updated `RegisterAsync` to hash passwords before saving.
    - Updated `LoginAsync` to hash passwords before comparison.
    - Added temporary condition in `LoginAsync` with a TODO comment.

--- BE_Project_29-07_02-08/Services/Auth/AuthService.cs
+++ BE_Project_29-07_02-08/Services/Auth/AuthService.cs
@@ -1,6 +1,8 @@
 ﻿using BE_Project_29_07_02_08.Context;
 using BE_Project_29_07_02_08.Models;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace BE_Project_29_07_02_08.Services.Auth
 {
@@ -12,8 +14,18 @@ namespace BE_Project_29_07_02_08.Services.Auth
             _dataContext = dataContext;
         }
 
+        //todo: move this to a helper class
+        private static string HashPassword(string password)
+        {
+            using (var sha256 = SHA256.Create())
+            {
+                var bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(password));
+                return Convert.ToBase64String(bytes);
+            }
+        }
         public async Task<User> RegisterAsync(User user)
         {
+            user.Password = HashPassword(user.Password);
             var userRole = await _dataContext.Roles.Where(r => r.IdRole == 2).FirstOrDefaultAsync();
             user.Roles.Add(userRole);
             await _dataContext.Users.AddAsync(user);
@@ -24,9 +36,12 @@ namespace BE_Project_29_07_02_08.Services.Auth
 
         public async Task<User> LoginAsync(User user)
         {
+            string hashedPassword = HashPassword(user.Password);
+
             var existingUser = await _dataContext.Users
                  .Include(u => u.Roles)
-                 .Where(u => u.Username == user.Username && u.Password == user.Password).FirstOrDefaultAsync();
+                 .Where(u => u.Username == user.Username && u.Password == hashedPassword || u.Password == user.Password) //todo: remove u.Password == user.Password 
+                 .FirstOrDefaultAsync();
 
             if (existingUser == null)
             {
